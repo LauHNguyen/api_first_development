d30c42088c3d7dff88fcbe032e1a058f2e133e84
LauHNguyen <hoannguyen.lau@gmail.com>
2025-07-05 17:47:07 +0700

    update API doc

--- generate-api.sh
+++ generate-api.sh
@@ -3,8 +3,6 @@
 # Check if LAMBDA_URI is set
 if [ -z "$LAMBDA_URI" ]; then
   echo "Please export LAMBDA_URI before running this script."
-  echo "Example:"
-  echo "  export LAMBDA_URI='arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:xxxx:function:UserService/invocations'"
   exit 1
 fi
 
